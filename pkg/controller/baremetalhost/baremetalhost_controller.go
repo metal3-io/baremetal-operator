8ff1649935dad4af0f1cab6c17e161ddfca31af5
Doug Hellmann <doug@doughellmann.com>
2019-02-05 15:08:47 -0500

    code cleanup
    Reformat comments and code without making other significant changes.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- pkg/controller/baremetalhost/baremetalhost_controller.go
+++ pkg/controller/baremetalhost/baremetalhost_controller.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	metalkubev1alpha1 "github.com/metalkube/baremetal-operator/pkg/apis/metalkube/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -19,13 +18,9 @@ import (
 
 var log = logf.Log.WithName("controller_baremetalhost")
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
-// Add creates a new BareMetalHost Controller and adds it to the Manager. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
+// Add creates a new BareMetalHost Controller and adds it to the
+// Manager. The Manager will set fields on the Controller and Start it
+// when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
 }
@@ -38,23 +33,15 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("baremetalhost-controller", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New("baremetalhost-controller", mgr,
+		controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
 	// Watch for changes to primary resource BareMetalHost
-	err = c.Watch(&source.Kind{Type: &metalkubev1alpha1.BareMetalHost{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner BareMetalHost
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &metalkubev1alpha1.BareMetalHost{},
-	})
+	err = c.Watch(&source.Kind{Type: &metalkubev1alpha1.BareMetalHost{}},
+		&handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
@@ -72,15 +59,17 @@ type ReconcileBareMetalHost struct {
 	scheme *runtime.Scheme
 }
 
-// Reconcile reads that state of the cluster for a BareMetalHost object and makes changes based on the state read
-// and what is in the BareMetalHost.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
-// Note:
-// The Controller will requeue the Request to be processed again if the returned error is non-nil or
-// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+// Reconcile reads that state of the cluster for a BareMetalHost
+// object and makes changes based on the state read and what is in the
+// BareMetalHost.Spec TODO(user): Modify this Reconcile function to
+// implement your Controller logic.  This example creates a Pod as an
+// example Note: The Controller will requeue the Request to be
+// processed again if the returned error is non-nil or Result.Requeue
+// is true, otherwise upon completion it will remove the work from the
+// queue.
 func (r *ReconcileBareMetalHost) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger := log.WithValues("Request.Namespace",
+		request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling BareMetalHost")
 
 	saveStatus := false
@@ -90,9 +79,10 @@ func (r *ReconcileBareMetalHost) Reconcile(request reconcile.Request) (reconcile
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
+			// Request object not found, could have been deleted after
+			// reconcile request.  Owned objects are automatically
+			// garbage collected. For additional cleanup logic use
+			// finalizers.  Return and don't requeue
 			return reconcile.Result{}, nil
 		}
 		// Error reading the object - requeue the request.
@@ -101,9 +91,6 @@ func (r *ReconcileBareMetalHost) Reconcile(request reconcile.Request) (reconcile
 
 	reqLogger.Info("found BMH instance", "instance", instance)
 
-	// Handle deletes
-
-	// If object hasn't been deleted and doesn't have a finalizer, add one
 	// Add a finalizer to newly created objects.
 	if instance.ObjectMeta.DeletionTimestamp.IsZero() &&
 		!stringInList(instance.ObjectMeta.Finalizers, metalkubev1alpha1.BareMetalHostFinalizer) {
@@ -121,6 +108,7 @@ func (r *ReconcileBareMetalHost) Reconcile(request reconcile.Request) (reconcile
 		}
 	}
 
+	// Handle delete operations.
 	if !instance.ObjectMeta.DeletionTimestamp.IsZero() {
 		reqLogger.Info(
 			"marked to be deleted",
@@ -128,7 +116,7 @@ func (r *ReconcileBareMetalHost) Reconcile(request reconcile.Request) (reconcile
 		)
 		// no-op if finalizer has been removed.
 		if !stringInList(instance.ObjectMeta.Finalizers, metalkubev1alpha1.BareMetalHostFinalizer) {
-			reqLogger.Info("reconciling BareMetalHost object causes a no-op as there is no finalizer")
+			reqLogger.Info("BareMetalHost is ready to be deleted")
 			return reconcile.Result{}, nil
 		}
 

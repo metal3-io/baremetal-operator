c1a98e4cce6770c37e3dfd196eda18cee8f19e8e
Doug Hellmann <dhellmann@redhat.com>
2019-02-06 17:44:38 -0500

    move string list functions to utils module
    Create a new module with the string list functions so they are easier
    to reuse.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

---
+++ pkg/utils/stringlist.go
@@ -0,0 +1,19 @@
+package utils
+
+func StringInList(list []string, strToSearch string) bool {
+	for _, item := range list {
+		if item == strToSearch {
+			return true
+		}
+	}
+	return false
+}
+
+func FilterStringFromList(list []string, strToFilter string) (newList []string) {
+	for _, item := range list {
+		if item != strToFilter {
+			newList = append(newList, item)
+		}
+	}
+	return
+}

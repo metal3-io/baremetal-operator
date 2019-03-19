11e177034910e87a93118e0fb7a70071d91c182c
Doug Hellmann <dhellmann@redhat.com>
2019-03-19 13:24:08 -0400

    dev mode enhancements
    Add "make run" target to simplify running the operator locally.
    
    Add a -dev switch to turn on "development mode", which changes the
    logging format to something a little easier for a human to parse.

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -28,13 +28,14 @@ func printVersion() {
 }
 
 func main() {
+	devLogging := flag.Bool("dev", false, "enable dev logging")
 	flag.Parse()
 
 	// The logger instantiated here can be changed to any logger
 	// implementing the logr.Logger interface. This logger will
 	// be propagated through the whole operator, generating
 	// uniform and structured logs.
-	logf.SetLogger(logf.ZapLogger(false))
+	logf.SetLogger(logf.ZapLogger(*devLogging))
 
 	printVersion()
 

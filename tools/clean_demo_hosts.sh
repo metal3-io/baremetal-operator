a357bd9045402c9e301d365c51ced78db3cf145c
Doug Hellmann <dhellmann@redhat.com>
2019-04-01 15:05:11 -0400

    label all demo hosts for more robust cleanup
    Instead of relying on the names of the hosts, add a label that we can
    use in the cleanup script to find them again.

--- tools/clean_demo_hosts.sh
+++ tools/clean_demo_hosts.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -x
 
-for host in $(oc get baremetalhosts --no-headers | grep '^demo-' | awk '{print $1}')
-do
-    oc delete baremetalhost $host
-done
+oc delete baremetalhost -l metalkubedemo

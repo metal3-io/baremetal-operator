a94536c6c0fe48d90c039ecf990948e932b39838
Zane Bitter <zbitter@redhat.com>
2019-04-05 16:03:49 -0400

    Support changing the provisioned image
    Previously to change the provisioned image, you would have to first set
    the image URL to "" and wait for deprovisioning to complete before
    reprovisioning with a new image URL. With this change, a change to the
    image URL will cause the server to be deprovisioned and reprovisioned
    with the new image.
    
    Signed-off-by: Zane Bitter <zbitter@redhat.com>

--- pkg/apis/metalkube/v1alpha1/baremetalhost_types.go
+++ pkg/apis/metalkube/v1alpha1/baremetalhost_types.go
@@ -413,8 +413,6 @@ func (host *BareMetalHost) NeedsProvisioning() bool {
 		// We have an image set, but not provisioned.
 		return true
 	}
-	// FIXME(dhellmann): Compare the provisioned image against the one
-	// we are supposed to have to make sure they match.
 	return false
 }
 
@@ -427,7 +425,7 @@ func (host *BareMetalHost) NeedsDeprovisioning() bool {
 	if host.Spec.Image == nil {
 		return true
 	}
-	if host.Spec.Image.URL == "" {
+	if host.Spec.Image.URL != host.Status.Provisioning.Image.URL {
 		return true
 	}
 	return false

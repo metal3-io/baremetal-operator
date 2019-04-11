b5daaf8d88835cc3bdb7a412326fddd8011c6521
Yurii Prokulevych <yprokule@redhat.com>
2019-04-11 11:57:48 +0200

    Update make-bm-worker to address use case from pr#330
    Few flags are missing causing an error while cr preparation, e.g.:
      flag provided but not defined: -machine-namespace
    
    [0] https://github.com/openshift-metalkube/dev-scripts/pull/330

--- cmd/make-bm-worker/main.go
+++ cmd/make-bm-worker/main.go
@@ -29,14 +29,24 @@ spec:
   bmc:
     address: {{ .BMCAddress }}
     credentialsName: {{ .Name }}-bmc-secret
+  bootMACAddress: {{ .MAC }}
+{{- if .WithMachine }}
+  machineRef:
+    name: {{ .Machine }}
+    namespace: {{ .MachineNamespace }}
+{{- end }}
 `
 
 // TemplateArgs holds the arguments to pass to the template.
 type TemplateArgs struct {
-	Name            string
-	BMCAddress      string
-	EncodedUsername string
-	EncodedPassword string
+	Name             string
+	BMCAddress       string
+	EncodedUsername  string
+	EncodedPassword  string
+	MAC              string
+	WithMachine      bool
+	Machine          string
+	MachineNamespace string
 }
 
 func encodeToSecret(input string) string {
@@ -48,6 +58,11 @@ func main() {
 	var password = flag.String("password", "", "password for BMC")
 	var bmcAddress = flag.String("address", "", "address URL for BMC")
 	var verbose = flag.Bool("v", false, "turn on verbose output")
+	var machine = flag.String(
+		"machine", "", "specify name of a related, existing, machine to link")
+	var machineNamespace = flag.String(
+		"machine-namespace", "", "specify namespace of a related, existing, machine to link")
+	var bootmac = flag.String("boot-mac", "", "MAC address of boot interface")
 
 	flag.Parse()
 
@@ -68,12 +83,22 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Missing -address argument\n")
 		os.Exit(1)
 	}
+	if *bootmac == "" {
+		fmt.Fprintf(os.Stderr, "Missing -boot-mac argument\n")
+		os.Exit(1)
+	}
 
 	args := TemplateArgs{
-		Name:            strings.Replace(hostName, "_", "-", -1),
-		BMCAddress:      *bmcAddress,
-		EncodedUsername: encodeToSecret(*username),
-		EncodedPassword: encodeToSecret(*password),
+		Name:             strings.Replace(hostName, "_", "-", -1),
+		BMCAddress:       strings.TrimSpace(*bmcAddress),
+		EncodedUsername:  encodeToSecret(*username),
+		EncodedPassword:  encodeToSecret(*password),
+		MAC:              strings.TrimSpace(*bootmac),
+		Machine:          strings.TrimSpace(*machine),
+		MachineNamespace: strings.TrimSpace(*machineNamespace),
+	}
+	if args.Machine != "" {
+		args.WithMachine = true
 	}
 	if *verbose {
 		fmt.Fprintf(os.Stderr, "%v", args)

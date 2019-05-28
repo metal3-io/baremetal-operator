f10b0ba58eb14f23a3819347d9cbf55719885b2d
Mark McLoughlin <markmc@redhat.com>
2019-05-28 06:31:17 +0100

    e2e: try a really long timeout
    Still timing out waiting for inspection to complete.

--- test/e2e/baremetal_test.go
+++ test/e2e/baremetal_test.go
@@ -33,7 +33,7 @@ import (
 
 var (
 	retryInterval        = time.Second * 5
-	timeout              = time.Second * 10
+	timeout              = time.Second * 480
 	cleanupRetryInterval = time.Second * 1
 	cleanupTimeout       = time.Second * 5
 )

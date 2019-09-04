b0f59f18ba0a10ef47190e4f4bc10e4c5edb737f
Doug Hellmann <dhellmann@redhat.com>
2019-09-04 14:38:28 -0400

    handle adoption during deprovisioning
    This change updates the ironic provisioner to handle adopting a host
    by recreating it in ironic when the ironic database is wiped while the
    host is being deprovisioned.
    
    The changes are limited to the ironic package because the more general
    adoption logic that works for hosts in steady states does not work for
    an active state like deprovisioning.
    
    The first aspect of the change is to ensure that when a host is
    registered with ironic, we do as much as possible to ensure it has an
    image source. In particular, if there is no Spec.Image because the
    host is being deprovisioned, we use the Status.Provisioning.Image
    values if they are set. This allows us to register the host with
    ironic and then trigger deprovisioning, which is not possible if there
    is no image source set.
    
    The second aspect of the change is the handling of the ironic node
    states in Deprovision(). The switch statement there is updated to
    include the states a node goes through as it is being adopted,
    including the fact that it will end up in the "manageable" state for
    two different reasons that must be handled separately.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- pkg/provisioner/ironic/ironic.go
+++ pkg/provisioner/ironic/ironic.go
@@ -277,24 +277,38 @@ func (p *ironicProvisioner) ValidateManagementAccess(credentialsChanged bool) (r
 			}
 		}
 
-		if p.host.Spec.Image != nil && p.host.Spec.Image.URL != "" {
-			// FIXME(dhellmann): The Stein version of Ironic supports passing
-			// a URL. When we upgrade, we can stop doing this work ourself.
-			checksum, err := p.getImageChecksum()
-			if err != nil {
-				return result, errors.Wrap(err, "failed to retrieve image checksum")
-			}
+		// If there is an image to be provisioned, or an image has
+		// previously been provisioned, include those details. Either
+		// case may mean we are re-adopting a host that was already
+		// known but removed/lost because the pod restarted.
+		//
+		// FIXME(dhellmann): The Stein version of Ironic supports
+		// passing a URL for the checksum. When we upgrade, we can
+		// stop looking up that value ourself.
+		var checksum string
+		var imageURL string
+		switch {
+		case p.host.Spec.Image != nil && p.host.Spec.Image.URL != "":
+			imageURL = p.host.Spec.Image.URL
+			checksum, err = p.getImageChecksum(p.host.Spec.Image.Checksum)
+		case p.host.Status.Provisioning.Image.URL != "":
+			imageURL = p.host.Status.Provisioning.Image.URL
+			checksum, err = p.getImageChecksum(p.host.Status.Provisioning.Image.Checksum)
+		}
+		if err != nil {
+			return result, errors.Wrap(err, "failed to retrieve image checksum")
+		}
 
+		if imageURL != "" {
 			p.log.Info("setting instance info",
-				"image_source", p.host.Spec.Image.URL,
+				"image_source", imageURL,
 				"checksum", checksum,
 			)
-
 			updates := nodes.UpdateOpts{
 				nodes.UpdateOperation{
 					Op:    nodes.AddOp,
 					Path:  "/instance_info/image_source",
-					Value: p.host.Spec.Image.URL,
+					Value: imageURL,
 				},
 				nodes.UpdateOperation{
 					Op:    nodes.AddOp,
@@ -676,8 +690,7 @@ func checksumIsURL(checksumURL string) (bool, error) {
 	return parsedChecksumURL.Scheme != "", nil
 }
 
-func (p *ironicProvisioner) getImageChecksum() (string, error) {
-	checksum := p.host.Spec.Image.Checksum
+func (p *ironicProvisioner) getImageChecksum(checksum string) (string, error) {
 	isURL, err := checksumIsURL(checksum)
 	if err != nil {
 		return "", errors.Wrap(err, "Could not understand image checksum")
@@ -967,7 +980,7 @@ func (p *ironicProvisioner) Provision(getUserData provisioner.UserDataSource) (r
 
 	// FIXME(dhellmann): The Stein version of Ironic supports passing
 	// a URL. When we upgrade, we can stop doing this work ourself.
-	checksum, err := p.getImageChecksum()
+	checksum, err := p.getImageChecksum(p.host.Spec.Image.Checksum)
 	if err != nil {
 		return result, errors.Wrap(err, "failed to retrieve image checksum")
 	}
@@ -1105,8 +1118,14 @@ func (p *ironicProvisioner) Deprovision() (result provisioner.Result, err error)
 		return result, errors.Wrap(err, "failed to find existing host")
 	}
 	if ironicNode == nil {
-		p.log.Info("no node found, already deleted")
-		return result, nil
+		// The node does not exist, but we were called so the
+		// controller thinks that the node existed at one time. That
+		// likely means data loss from restarting the database, so
+		// pass through the validation process to register the node
+		// again. Pass true to indicate that we need to re-test the
+		// credentials, just in case.
+		p.log.Info("re-registering host")
+		return p.ValidateManagementAccess(true)
 	}
 
 	p.log.Info("deprovisioning host",
@@ -1115,11 +1134,12 @@ func (p *ironicProvisioner) Deprovision() (result provisioner.Result, err error)
 		"current", ironicNode.ProvisionState,
 		"target", ironicNode.TargetProvisionState,
 		"deploy step", ironicNode.DeployStep,
+		"instance_info", ironicNode.InstanceInfo,
 	)
 
 	switch nodes.ProvisionState(ironicNode.ProvisionState) {
 
-	case nodes.Error, nodes.CleanFail:
+	case nodes.Error, nodes.CleanFail, nodes.AdoptFail:
 		if !ironicNode.Maintenance {
 			p.log.Info("setting host maintenance flag to force image delete")
 			return p.setMaintenanceFlag(ironicNode, true)
@@ -1175,17 +1195,43 @@ func (p *ironicProvisioner) Deprovision() (result provisioner.Result, err error)
 		result.RequeueAfter = deprovisionRequeueDelay
 		return result, nil
 
-	case nodes.Manageable, nodes.Enroll, nodes.Verifying:
+	case nodes.Enroll, nodes.Verifying, nodes.Adopting:
+		p.log.Info("re-registering to deprovision")
+		result.Dirty = true
+		result.RequeueAfter = deprovisionRequeueDelay
+		return result, nil
+
+	case nodes.Manageable:
+		// We can end up "manageable" under two conditions.  If we end
+		// up manageable because of a successful deprovisioning
+		// operation, when the node moves from cleaning to manageable,
+		// the instance_info structure is cleared out so we know we
+		// are done. If we end up here because we re-register a host,
+		// moving from verifying to manageable, then we have an image
+		// source and we need to adopt the node so we can try to
+		// deprovision all over again.
+		if _, ok := ironicNode.InstanceInfo["image_source"]; ok {
+			p.log.Info("adopting to deprovision")
+			return p.changeNodeProvisionState(
+				ironicNode,
+				nodes.ProvisionStateOpts{
+					Target: nodes.TargetAdopt,
+				},
+			)
+		}
 		p.publisher("DeprovisioningComplete", "Image deprovisioning completed")
 		return result, nil
 
-	default:
+	case nodes.Active:
 		p.log.Info("starting deprovisioning")
 		p.publisher("DeprovisioningStarted", "Image deprovisioning started")
 		return p.changeNodeProvisionState(
 			ironicNode,
 			nodes.ProvisionStateOpts{Target: nodes.TargetDeleted},
 		)
+
+	default:
+		return result, fmt.Errorf("Unhandled ironic state %s", ironicNode.ProvisionState)
 	}
 }
 

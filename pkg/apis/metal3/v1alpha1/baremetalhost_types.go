d8985297ebc6306505b7a7af2efa20691df512dc
kashifest <kashif.khan@est.tech>
2020-05-14 19:47:41 +0300

    Add status annotation documentation

--- pkg/apis/metal3/v1alpha1/baremetalhost_types.go
+++ pkg/apis/metal3/v1alpha1/baremetalhost_types.go
@@ -23,7 +23,10 @@ const (
 	// an immediate requeue)
 	PausedAnnotation = "baremetalhost.metal3.io/paused"
 
-	// StatusAnnotation is the annotation that holds the Status of BMH
+	// StatusAnnotation is the annotation that keeps a copy of the Status of BMH
+	// This is particularly useful when we pivot BMH. If the status
+	// annotation is present and status is empty, BMO will reconstruct BMH Status
+	// from the status annotation.
 	StatusAnnotation = "baremetalhost.metal3.io/status"
 )
 

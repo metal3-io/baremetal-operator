debce7f43c9f20d10e00a375a1314d80b9b53c25
Doug Hellmann <dhellmann@redhat.com>
2020-07-01 17:47:09 -0400

    add AddToScheme
    The client code generate expects the AddToScheme symbol to be defined
    in the way that kubebuilder generates it.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- pkg/apis/metal3/v1alpha1/register.go
+++ pkg/apis/metal3/v1alpha1/register.go
@@ -16,4 +16,7 @@ var (
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	// AddToScheme is used in the generated client code
+	AddToScheme = SchemeBuilder.AddToScheme
 )

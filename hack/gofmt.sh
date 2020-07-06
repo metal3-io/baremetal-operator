328f7a85290e0e34841c4e84b01de0385902db81
Doug Hellmann <dhellmann@redhat.com>
2020-07-06 16:00:05 -0400

    update hack scripts to all use golang 1.13.7 for consistency
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- hack/gofmt.sh
+++ hack/gofmt.sh
@@ -22,6 +22,6 @@ else
     --volume "${PWD}:/workdir:rw,z" \
     --entrypoint sh \
     --workdir /workdir \
-    registry.hub.docker.com/library/golang:1.12 \
+    registry.hub.docker.com/library/golang:1.13.7 \
     /workdir/hack/gofmt.sh "${@}"
 fi;

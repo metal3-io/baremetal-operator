1c8a0a705c6e37799bca6451d3f8a8c8275ce8d5
Doug Hellmann <dhellmann@redhat.com>
2020-07-08 16:55:47 -0400

    fix hack/unit.sh
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- hack/unit.sh
+++ hack/unit.sh
@@ -9,15 +9,21 @@ CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
 if [ "${IS_CONTAINER}" != "false" ]; then
   eval "$(go env)"
   cd "${GOPATH}"/src/github.com/metal3-io/baremetal-operator
+
   export XDG_CACHE_HOME="/tmp/.cache"
-  go test -v ./pkg/... ./cmd/... -coverprofile "${ARTIFACTS}"/cover.out
+  export DEPLOY_KERNEL_URL=http://172.22.0.1/images/ironic-python-agent.kernel \
+  export DEPLOY_RAMDISK_URL=http://172.22.0.1/images/ironic-python-agent.initramfs \
+  export IRONIC_ENDPOINT=http://localhost:6385/v1/ \
+  export IRONIC_INSPECTOR_ENDPOINT=http://localhost:5050/v1/ \
+
+  # download kubebuilder and extract it to tmp
+  curl -L https://go.kubebuilder.io/dl/2.3.1/${GOOS}/${GOARCH} | tar -xz -C /tmp/
+  mv /tmp/kubebuilder_2.3.1_${GOOS}_${GOARCH} /usr/local/kubebuilder
+
+  go test ./... -coverprofile "${ARTIFACTS}"/cover.out
 else
   "${CONTAINER_RUNTIME}" run --rm \
     --env IS_CONTAINER=TRUE \
-    --env DEPLOY_KERNEL_URL=http://172.22.0.1/images/ironic-python-agent.kernel \
-    --env DEPLOY_RAMDISK_URL=http://172.22.0.1/images/ironic-python-agent.initramfs \
-    --env IRONIC_ENDPOINT=http://localhost:6385/v1/ \
-    --env IRONIC_INSPECTOR_ENDPOINT=http://localhost:5050/v1/ \
     --volume "${PWD}:/go/src/github.com/metal3-io/baremetal-operator:rw,z" \
     --entrypoint sh \
     --workdir /go/src/github.com/metal3-io/baremetal-operator \

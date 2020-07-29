b697640b588da29629a101feef8c51970cda8149
Zane Bitter <zbitter@redhat.com>
2020-07-29 16:17:39 -0400

    get-hardware-details: Refactor CLI options parsing
    Move the CLI parsing out of the main() function.

--- cmd/get-hardware-details/main.go
+++ cmd/get-hardware-details/main.go
@@ -12,36 +12,22 @@ import (
 	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/hardwaredetails"
 )
 
-func main() {
-	var nodeID string
-	authConfig := clients.AuthConfig{
-		Type: clients.AuthType(os.Getenv("IRONIC_AUTH_STRATEGY")),
-	}
+type options struct {
+	Endpoint   string
+	AuthConfig clients.AuthConfig
+	NodeID     string
+}
 
-	switch authConfig.Type {
-	case clients.HTTPBasicAuth:
-		if len(os.Args) != 5 {
-			fmt.Println("Usage: get-hardware-details <inspector URI> <inspector User> <inspector Password> <node UUID>")
-			os.Exit(1)
-		}
-		authConfig.Username = os.Args[2]
-		authConfig.Password = os.Args[3]
-		nodeID = os.Args[4]
-	default:
-		if len(os.Args) != 3 {
-			fmt.Println("Usage: get-hardware-details <inspector URI> <node UUID>")
-			os.Exit(1)
-		}
-		nodeID = os.Args[2]
-	}
-	inspectorEndpoint := os.Args[1]
-	inspector, err := clients.InspectorClient(inspectorEndpoint, authConfig)
+func main() {
+	opts := getOptions()
+
+	inspector, err := clients.InspectorClient(opts.Endpoint, opts.AuthConfig)
 	if err != nil {
 		fmt.Printf("could not get inspector client: %s", err)
 		os.Exit(1)
 	}
 
-	introData := introspection.GetIntrospectionData(inspector, nodeID)
+	introData := introspection.GetIntrospectionData(inspector, opts.NodeID)
 	data, err := introData.Extract()
 	if err != nil {
 		fmt.Printf("could not get introspection data: %s", err)
@@ -55,4 +41,27 @@ func main() {
 	}
 
 	fmt.Println(string(json))
+}
+
+func getOptions() (o options) {
+	o.AuthConfig.Type = clients.AuthType(os.Getenv("IRONIC_AUTH_STRATEGY"))
+
+	switch o.AuthConfig.Type {
+	case clients.HTTPBasicAuth:
+		if len(os.Args) != 5 {
+			fmt.Println("Usage: get-hardware-details <inspector URI> <inspector User> <inspector Password> <node UUID>")
+			os.Exit(1)
+		}
+		o.AuthConfig.Username = os.Args[2]
+		o.AuthConfig.Password = os.Args[3]
+		o.NodeID = os.Args[4]
+	default:
+		if len(os.Args) != 3 {
+			fmt.Println("Usage: get-hardware-details <inspector URI> <node UUID>")
+			os.Exit(1)
+		}
+		o.NodeID = os.Args[2]
+	}
+	o.Endpoint = os.Args[1]
+	return
 }

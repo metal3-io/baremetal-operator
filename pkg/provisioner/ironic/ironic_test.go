c71a2512932f3765b4569e54c1394f33dd251d85
Doug Hellmann <dhellmann@redhat.com>
2020-09-03 15:52:27 -0400

    move tests for ready status to their own file
    add error handling and some other cleanup at the same time
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- pkg/provisioner/ironic/ironic_test.go
+++ pkg/provisioner/ironic/ironic_test.go
@@ -2,7 +2,6 @@ package ironic
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,120 +14,11 @@ import (
 	metal3v1alpha1 "github.com/metal3-io/baremetal-operator/pkg/apis/metal3/v1alpha1"
 	"github.com/metal3-io/baremetal-operator/pkg/bmc"
 	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/clients"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/testserver"
 	"k8s.io/utils/pointer"
 )
 
 func init() {
 	logf.SetLogger(logf.ZapLogger(true))
-}
-
-func TestProvisionerIsReady(t *testing.T) {
-
-	cases := []struct {
-		name      string
-		ironic    *testserver.MockServer
-		inspector *testserver.MockServer
-
-		expectedIronicCalls    string
-		expectedInspectorCalls string
-		expectedIsReady        bool
-		expectedError          string
-	}{
-		{
-			name:      "IsReady",
-			ironic:    testserver.New(t, "ironic").AddDrivers(),
-			inspector: testserver.New(t, "inspector"),
-
-			expectedIronicCalls:    "/v1;/v1/drivers;",
-			expectedInspectorCalls: "/v1;",
-			expectedIsReady:        true,
-		},
-		{
-			name:      "NoDriversLoaded",
-			ironic:    testserver.New(t, "ironic"),
-			inspector: testserver.New(t, "inspector"),
-
-			expectedIronicCalls: "/v1;/v1/drivers;",
-		},
-		{
-			name:      "IronicDown",
-			inspector: testserver.New(t, "inspector"),
-
-			expectedIsReady: false,
-		},
-		{
-			name:   "InspectorDown",
-			ironic: testserver.New(t, "ironic").AddDrivers(),
-
-			expectedIronicCalls: "/v1;/v1/drivers;",
-
-			expectedIsReady: false,
-		},
-		{
-			name:      "IronicNotOk",
-			ironic:    testserver.New(t, "ironic").SetErrorCode(http.StatusInternalServerError),
-			inspector: testserver.New(t, "inspector"),
-
-			expectedIsReady: false,
-
-			expectedIronicCalls: "/v1;",
-		},
-		{
-			name:      "IronicNotOkAndNotExpected",
-			ironic:    testserver.New(t, "ironic").SetErrorCode(http.StatusBadGateway),
-			inspector: testserver.New(t, "inspector"),
-
-			expectedIsReady: false,
-
-			expectedIronicCalls: "/v1;",
-		},
-		{
-			name:      "InspectorNotOk",
-			ironic:    testserver.New(t, "ironic").AddDrivers(),
-			inspector: testserver.New(t, "inspector").SetErrorCode(http.StatusInternalServerError),
-
-			expectedIsReady: false,
-
-			expectedIronicCalls:    "/v1;/v1/drivers;",
-			expectedInspectorCalls: "/v1;",
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			if tc.ironic != nil {
-				tc.ironic.Start()
-				defer tc.ironic.Stop()
-			}
-
-			if tc.inspector != nil {
-				tc.inspector.Start()
-				defer tc.inspector.Stop()
-			}
-
-			auth := clients.AuthConfig{Type: clients.NoAuth}
-
-			prov, err := newProvisionerWithSettings(makeHost(), bmc.Credentials{}, nil,
-				tc.ironic.Endpoint(), auth, tc.inspector.Endpoint(), auth,
-			)
-			ready, err := prov.IsReady()
-
-			if tc.ironic != nil {
-				assert.Equal(t, tc.expectedIronicCalls, tc.ironic.Requests)
-			}
-			if tc.inspector != nil {
-				assert.Equal(t, tc.expectedInspectorCalls, tc.inspector.Requests)
-			}
-
-			if tc.expectedError != "" {
-				assert.Regexp(t, tc.expectedError, err)
-			} else {
-				assert.Nil(t, err)
-				assert.Equal(t, tc.expectedIsReady, ready)
-			}
-		})
-	}
 }
 
 func TestGetUpdateOptsForNodeWithRootHints(t *testing.T) {

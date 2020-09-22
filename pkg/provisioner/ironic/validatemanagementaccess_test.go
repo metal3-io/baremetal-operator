4dd9c742a6727566faf71e428ec5e727a136ef5f
Doug Hellmann <dhellmann@redhat.com>
2020-09-22 17:50:47 -0400

    ensure ironic provisioner makes boot mac optional when driver does
    Verify that ValidateManagementAccess() does not report an error when
    the host has no boot MAC and the BMC does not require one.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- pkg/provisioner/ironic/validatemanagementaccess_test.go
+++ pkg/provisioner/ironic/validatemanagementaccess_test.go
@@ -3,6 +3,7 @@ package ironic
 import (
 	"testing"
 
+	"github.com/gophercloud/gophercloud/openstack/baremetal/v1/nodes"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/metal3-io/baremetal-operator/pkg/bmc"
@@ -35,4 +36,34 @@ func TestValidateManagementAccessNoMAC(t *testing.T) {
 		t.Fatalf("error from ValidateManagementAccess: %s", err)
 	}
 	assert.Contains(t, result.ErrorMessage, "requires a BootMACAddress")
+}
+
+func TestValidateManagementAccessMACOptional(t *testing.T) {
+	// Create a host without a bootMACAddress and with a BMC that
+	// does not require one.
+	host := makeHost()
+	host.Spec.BootMACAddress = ""
+
+	// Set up ironic server to return the node
+	ironic := testserver.NewIronic(t).Ready().
+		WithNode(nodes.Node{
+			Name: host.Name,
+			UUID: host.Status.Provisioning.ID,
+		})
+	ironic.Start()
+	defer ironic.Stop()
+
+	auth := clients.AuthConfig{Type: clients.NoAuth}
+	prov, err := newProvisionerWithSettings(host, bmc.Credentials{}, nil,
+		ironic.Endpoint(), auth, testserver.NewInspector(t).Endpoint(), auth,
+	)
+	if err != nil {
+		t.Fatalf("could not create provisioner: %s", err)
+	}
+
+	result, err := prov.ValidateManagementAccess(false)
+	if err != nil {
+		t.Fatalf("error from ValidateManagementAccess: %s", err)
+	}
+	assert.Equal(t, "", result.ErrorMessage)
 }

9b157723441814824c2948eeae0263c68eefe167
Doug Hellmann <dhellmann@redhat.com>
2020-09-23 17:13:51 -0400

    configure the concurrency option for the controller
    Move the environment variable check from init() to the point where the
    value of the variable would be used.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- controllers/metal3.io/baremetalhost_controller.go
+++ controllers/metal3.io/baremetalhost_controller.go
@@ -18,11 +18,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	metal3iov1alpha1 "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
 	"github.com/metal3-io/baremetal-operator/pkg/provisioner"
@@ -39,6 +44,7 @@ type BareMetalHostReconciler struct {
 // +kubebuilder:rbac:groups=metal3.io,resources=baremetalhosts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=metal3.io,resources=baremetalhosts/status,verbs=get;update;patch
 
+// Reconcile is called when a host changes.
 func (r *BareMetalHostReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
 	_ = r.Log.WithValues("baremetalhost", req.NamespacedName)
@@ -48,8 +54,32 @@ func (r *BareMetalHostReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 	return ctrl.Result{}, nil
 }
 
+// SetupWithManager configures the reconciler to run as part of a
+// controller manager.
 func (r *BareMetalHostReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	maxConcurrentReconciles := 3
+	if mcrEnv, ok := os.LookupEnv("BMO_CONCURRENCY"); ok {
+		mcr, err := strconv.Atoi(mcrEnv)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("BMO_CONCURRENCY value: %s is invalid", mcrEnv))
+		}
+		if mcr > 0 {
+			ctrl.Log.Info(fmt.Sprintf("BMO_CONCURRENCY of %d is set via an environment variable", mcr))
+			maxConcurrentReconciles = mcr
+		} else {
+			ctrl.Log.Info(fmt.Sprintf("Invalid BMO_CONCURRENCY value. Operator Concurrency will be set to a default value of %d", maxConcurrentReconciles))
+		}
+	} else {
+		ctrl.Log.Info(fmt.Sprintf("Operator Concurrency will be set to a default value of %d", maxConcurrentReconciles))
+	}
+
+	opts := controller.Options{
+		MaxConcurrentReconciles: maxConcurrentReconciles,
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&metal3iov1alpha1.BareMetalHost{}).
+		WithOptions(opts).
 		Complete(r)
 }

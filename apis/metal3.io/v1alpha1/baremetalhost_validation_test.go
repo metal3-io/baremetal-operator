7182e32efb6845aec5071ce6d29a6af3370608f2
Arda Güçlü <aguclu@redhat.com>
2021-04-27 11:53:43 +0300

    Add hardwareRAIDVolumes and softwareRAIDVolumes same time validation

---
+++ apis/metal3.io/v1alpha1/baremetalhost_validation_test.go
@@ -0,0 +1,64 @@
+package v1alpha1
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestValidate(t *testing.T) {
+	tm := metav1.TypeMeta{
+		Kind:       "BareMetalHost",
+		APIVersion: "metal3.io/v1alpha1",
+	}
+
+	om := metav1.ObjectMeta{
+		Name:      "test",
+		Namespace: "test-namespace",
+	}
+
+	tests := []struct {
+		name      string
+		newBMH    *BareMetalHost
+		oldBMH    *BareMetalHost
+		wantedErr string
+	}{
+		{
+			name:      "valid",
+			newBMH:    &BareMetalHost{TypeMeta: tm, ObjectMeta: om, Spec: BareMetalHostSpec{}},
+			oldBMH:    nil,
+			wantedErr: "",
+		},
+		{
+			name: "invalidRAID",
+			newBMH: &BareMetalHost{TypeMeta: tm, ObjectMeta: om, Spec: BareMetalHostSpec{RAID: &RAIDConfig{
+				HardwareRAIDVolumes: []HardwareRAIDVolume{
+					{
+						SizeGibibytes:         nil,
+						Level:                 "",
+						Name:                  "",
+						Rotational:            nil,
+						NumberOfPhysicalDisks: nil,
+					},
+				},
+				SoftwareRAIDVolumes: []SoftwareRAIDVolume{
+					{
+						SizeGibibytes: nil,
+						Level:         "",
+						PhysicalDisks: nil,
+					},
+				},
+			}}},
+			oldBMH:    nil,
+			wantedErr: "hardwareRAIDVolumes and softwareRAIDVolumes can not be set at the same time",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.newBMH.Validate(tt.oldBMH); !errorContains(err, tt.wantedErr) {
+				t.Errorf("BareMetalHost.ValidateBareMetalHost() error = %v, wantErr %v", err, tt.wantedErr)
+			}
+		})
+	}
+}

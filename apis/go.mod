3d3bfdfa93e492f43081038bcbd237865438b8d1
Arda Guclu <aguclu@redhat.com>
2021-08-24 16:20:23 +0300

    Enable BareMetalHost validation webhook

--- apis/go.mod
+++ apis/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/stretchr/testify v1.7.0
 	k8s.io/api v0.21.1
 	k8s.io/apimachinery v0.21.1
+	k8s.io/client-go v0.21.1 // indirect
 	sigs.k8s.io/controller-runtime v0.9.0
 )
---
+++ apis/metal3.io/v1alpha1/baremetalhost_validation_test.go
@@ -0,0 +1,135 @@
+package v1alpha1
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func errorArrContains(out []error, want string) bool {
+	if out == nil {
+		return want == ""
+	}
+	if want == "" {
+		return false
+	}
+	for _, err := range out {
+		if err.Error() == want {
+			return true
+		}
+	}
+	return false
+}
+
+func TestValidateCreate(t *testing.T) {
+	tm := metav1.TypeMeta{
+		Kind:       "BareMetalHost",
+		APIVersion: "metal3.io/v1alpha1",
+	}
+
+	om := metav1.ObjectMeta{
+		Name:      "test",
+		Namespace: "test-namespace",
+	}
+
+	tests := []struct {
+		name      string
+		newBMH    *BareMetalHost
+		oldBMH    *BareMetalHost
+		wantedErr string
+	}{
+		{
+			name:      "valid",
+			newBMH:    &BareMetalHost{TypeMeta: tm, ObjectMeta: om, Spec: BareMetalHostSpec{}},
+			oldBMH:    nil,
+			wantedErr: "",
+		},
+		{
+			name: "invalidRAID",
+			newBMH: &BareMetalHost{
+				TypeMeta:   tm,
+				ObjectMeta: om,
+				Spec: BareMetalHostSpec{
+					RAID: &RAIDConfig{
+						HardwareRAIDVolumes: []HardwareRAIDVolume{
+							{
+								SizeGibibytes:         nil,
+								Level:                 "",
+								Name:                  "",
+								Rotational:            nil,
+								NumberOfPhysicalDisks: nil,
+							},
+						},
+						SoftwareRAIDVolumes: []SoftwareRAIDVolume{
+							{
+								SizeGibibytes: nil,
+								Level:         "",
+								PhysicalDisks: nil,
+							},
+						},
+					}}},
+			oldBMH:    nil,
+			wantedErr: "hardwareRAIDVolumes and softwareRAIDVolumes can not be set at the same time",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.newBMH.validateHost(); !errorArrContains(err, tt.wantedErr) {
+				t.Errorf("BareMetalHost.ValidateBareMetalHost() error = %v, wantErr %v", err, tt.wantedErr)
+			}
+		})
+	}
+}
+
+func TestValidateUpdate(t *testing.T) {
+	tm := metav1.TypeMeta{
+		Kind:       "BareMetalHost",
+		APIVersion: "metal3.io/v1alpha1",
+	}
+
+	om := metav1.ObjectMeta{
+		Name:      "test",
+		Namespace: "test-namespace",
+	}
+
+	tests := []struct {
+		name      string
+		newBMH    *BareMetalHost
+		oldBMH    *BareMetalHost
+		wantedErr string
+	}{
+		{
+			name: "updateAddress",
+			newBMH: &BareMetalHost{
+				TypeMeta:   tm,
+				ObjectMeta: om,
+				Spec: BareMetalHostSpec{
+					BMC: BMCDetails{
+						Address: "test-address-changed"}}},
+			oldBMH: &BareMetalHost{
+				TypeMeta:   tm,
+				ObjectMeta: om,
+				Spec: BareMetalHostSpec{
+					BMC: BMCDetails{
+						Address: "test-address"}}},
+			wantedErr: "BMC address can not be changed once it is set",
+		},
+		{
+			name: "updateBootMAC",
+			newBMH: &BareMetalHost{
+				TypeMeta: tm, ObjectMeta: om, Spec: BareMetalHostSpec{BootMACAddress: "test-mac-changed"}},
+			oldBMH: &BareMetalHost{
+				TypeMeta: tm, ObjectMeta: om, Spec: BareMetalHostSpec{BootMACAddress: "test-mac"}},
+			wantedErr: "bootMACAddress can not be changed once it is set",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.newBMH.validateChanges(tt.oldBMH); !errorArrContains(err, tt.wantedErr) {
+				t.Errorf("BareMetalHost.ValidateBareMetalHost() error = %v, wantErr %v", err, tt.wantedErr)
+			}
+		})
+	}
+}

8e3c3558e6d36395d6a118402be7db280678c863
Zhou Hao <zhouhao@fujitsu.com>
2021-11-18 16:27:31 +0800

    add UT for FilterStringFromList
    Signed-off-by: Zhou Hao <zhouhao@fujitsu.com>

--- pkg/utils/stringlist_test.go
+++ pkg/utils/stringlist_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -26,6 +27,33 @@ func TestStringInList(t *testing.T) {
 		got := StringInList(c.list, c.str)
 		if c.expect != got {
 			t.Errorf("Expected '%t', but got '%t'", c.expect, got)
+		}
+
+	}
+}
+
+func TestFilterStringFromList(t *testing.T) {
+	cases := []struct {
+		list      []string
+		str       string
+		expectStr []string
+	}{
+		{
+			list:      []string{"a", "b", "c"},
+			str:       "a",
+			expectStr: []string{"b", "c"},
+		},
+		{
+			list:      []string{"a", "b", "c"},
+			str:       "d",
+			expectStr: []string{"a", "b", "c"},
+		},
+	}
+
+	for _, c := range cases {
+		newList := FilterStringFromList(c.list, c.str)
+		if !reflect.DeepEqual(newList, c.expectStr) {
+			t.Errorf("Expected '%s', but got '%s'", c.expectStr, newList)
 		}
 
 	}

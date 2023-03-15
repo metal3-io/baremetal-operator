4908f57a0b3c70cf8c3cc6eb01270ceb1befa011
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2023-03-15 14:52:11 +0200

    run BMO deployment as non-root
    BMO ironic has no reason to run as root. Make it run as "ironic" user.
    
    dnsmasq requires elevated capabiities. k8s is missing the feature of
    ambient capabilities, so it requires us to setcap the binaries with
    expected capabilities and container must be running with
    "allowPrivilegeEscalation: true" in the manifest to allow elevation.
    
    Read the ambient capabilities KEP for more details:
    https://github.com/kubernetes/enhancements/blob/master/keps/sig-security/2763-ambient-capabilities/README.md
    
    Add securityContext to BMO deployment manifest and keepalived
    component, with correct UIDs and GIDs. This is important to be able
    to share files via /shared.
    
    Modify keepalived image to run as ironic user, which we use the same
    UID and GID as the ironic-image.
    
    This commit requires ironic-image with PR
    https://github.com/metal3-io/ironic-image/pull/410 to be merged to
    work.

--- resources/keepalived-docker/manage-keepalived.sh
+++ resources/keepalived-docker/manage-keepalived.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-export assignedIP="$PROVISIONING_IP/32"
-export interface=$PROVISIONING_INTERFACE
 
-sed  -i "s~INTERFACE~$interface~g" /etc/keepalived/keepalived.conf
-sed  -i "s~CHANGEIP~$assignedIP~g" /etc/keepalived/keepalived.conf
+set -eux
 
-exec /usr/sbin/keepalived -n -l
+export assignedIP="${PROVISIONING_IP}/32"
+export interface="${PROVISIONING_INTERFACE}"
+
+sed -i "s~INTERFACE~${interface}~g" /etc/keepalived/keepalived.conf
+sed -i "s~CHANGEIP~${assignedIP}~g" /etc/keepalived/keepalived.conf
+
+exec /usr/sbin/keepalived -n -l -p /run/keepalived/keepalived.pid -r /run/keepalived/vrrp.pid

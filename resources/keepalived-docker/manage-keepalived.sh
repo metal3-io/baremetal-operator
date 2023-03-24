35275a8d516a79685cf314aeb317f186b59ba5c2
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2023-03-24 10:45:24 +0200

    Revert "run BMO deployment as non-root"
    This reverts commit 4908f57a0b3c70cf8c3cc6eb01270ceb1befa011.
    
    We missed mariadb securityContext and we also have conflict in
    release branches related to unversioned keepalived image, which breaks
    the branches.

--- resources/keepalived-docker/manage-keepalived.sh
+++ resources/keepalived-docker/manage-keepalived.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+export assignedIP="$PROVISIONING_IP/32"
+export interface=$PROVISIONING_INTERFACE
 
-set -eux
+sed  -i "s~INTERFACE~$interface~g" /etc/keepalived/keepalived.conf
+sed  -i "s~CHANGEIP~$assignedIP~g" /etc/keepalived/keepalived.conf
 
-export assignedIP="${PROVISIONING_IP}/32"
-export interface="${PROVISIONING_INTERFACE}"
-
-sed -i "s~INTERFACE~${interface}~g" /etc/keepalived/keepalived.conf
-sed -i "s~CHANGEIP~${assignedIP}~g" /etc/keepalived/keepalived.conf
-
-exec /usr/sbin/keepalived -n -l -p /run/keepalived/keepalived.pid -r /run/keepalived/vrrp.pid
+exec /usr/sbin/keepalived -n -l

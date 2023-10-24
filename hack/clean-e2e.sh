bde3a8435682064b07a1b87810a6bc44bb719d87
Max Rantil <max.rantil@est.tech>
2023-10-24 18:27:44 +0000

    Implement BMH provisioning E2E test
    Introduce an end-to-end test validating the provisioning and
    deprovisioning flow of a BareMetalHost.

--- hack/clean-e2e.sh
+++ hack/clean-e2e.sh
@@ -4,11 +4,13 @@ REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${REPO_ROOT}" || exit 1
 
 minikube delete
-docker stop vbmc
-docker rm vbmc
+docker rm -f vbmc
+docker rm -f image-server-e2e
 virsh -c qemu:///system destroy --domain bmo-e2e-0
 virsh -c qemu:///system undefine --domain bmo-e2e-0 --remove-all-storage
 virsh -c qemu:///system net-destroy baremetal-e2e
 virsh -c qemu:///system net-undefine baremetal-e2e
 
-rm -rf "${REPO_ROOT}/test/e2e/_artifacts"
+rm -rfv "${REPO_ROOT}/test/e2e/_artifacts"
+rm -rfv "${REPO_ROOT}/artifacts.tar.gz"
+rm -rfv "${REPO_ROOT}/test/e2e/images"

a72ffa6024ae4b78235f0f141ae4dfb0d27492c5
Dmitry Tantsur <dtantsur@protonmail.com>
2023-11-03 10:08:27 +0100

    Clean up logging and error handling in the webhooks
    Logging:
    - Rename the webhook loggers for consistency and easier grepping
    - Remove redundant logging from the validation code
    
    Errors:
    - Provide wrapped errors in a consistent fashion
    - Do not refer to internal constant names in user-visible strings
    - Match field names to those in JSON (i.e. camelCase)
    - Provide allowed values when saying that something is not allowed
    - Use the stdlib error routines instead of the deprecated pkg/errors.
    - Do not shadow the errors package name by local variables

--- apis/metal3.io/v1alpha1/baremetalhost_webhook.go
+++ apis/metal3.io/v1alpha1/baremetalhost_webhook.go
@@ -23,22 +23,22 @@ import (
 )
 
 // log is for logging in this package.
-var baremetalhostlog = logf.Log.WithName("baremetalhost-resource")
+var baremetalhostlog = logf.Log.WithName("webhooks").WithName("BareMetalHost")
 
 //+kubebuilder:webhook:verbs=create;update,path=/validate-metal3-io-v1alpha1-baremetalhost,mutating=false,failurePolicy=fail,sideEffects=none,admissionReviewVersions=v1;v1beta,groups=metal3.io,resources=baremetalhosts,versions=v1alpha1,name=baremetalhost.metal3.io
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *BareMetalHost) ValidateCreate() (admission.Warnings, error) {
-	baremetalhostlog.Info("validate create", "name", r.Name)
+	baremetalhostlog.Info("validate create", "namespace", r.Namespace, "name", r.Name)
 	return nil, errors.NewAggregate(r.validateHost())
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *BareMetalHost) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
-	baremetalhostlog.Info("validate update", "name", r.Name)
+	baremetalhostlog.Info("validate update", "namespace", r.Namespace, "name", r.Name)
 	bmh, casted := old.(*BareMetalHost)
 	if !casted {
-		baremetalhostlog.Error(fmt.Errorf("old object conversion error"), "validate update error")
+		baremetalhostlog.Error(fmt.Errorf("old object conversion error for %s/%s", r.Namespace, r.Name), "validate update error")
 		return nil, nil
 	}
 	return nil, errors.NewAggregate(r.validateChanges(bmh))

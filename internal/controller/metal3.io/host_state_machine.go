ae84cae8e4a1747625b498b43aa4fb1c4965c046
Himanshu Roy <hroy@redhat.com>
2025-06-13 15:54:12 +0530

    directly skip to Deleting state for externallyProvisioned host when detached annotation is present
    Signed-off-by: Himanshu Roy <hroy@redhat.com>

--- internal/controller/metal3.io/host_state_machine.go
+++ internal/controller/metal3.io/host_state_machine.go
@@ -228,7 +228,7 @@ func (hsm *hostStateMachine) checkInitiateDelete(log logr.Logger) bool {
 	case metal3api.StateRegistering, metal3api.StateUnmanaged, metal3api.StateNone:
 		// Skip the power off before delete
 		hsm.NextState = metal3api.StateDeleting
-	case metal3api.StateProvisioning, metal3api.StateProvisioned:
+	case metal3api.StateProvisioning, metal3api.StateProvisioned, metal3api.StateExternallyProvisioned:
 		if hsm.Host.OperationalStatus() == metal3api.OperationalStatusDetached {
 			if delayDeleteForDetachedHost(hsm.Host) {
 				log.Info("Delaying detached host deletion")

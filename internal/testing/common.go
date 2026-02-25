cb02d2eada722200bec600dfe8ac1c3bd5c471e6
Pierre Crégut <pierre.cregut@orange.com>
2026-02-25 14:08:58 +0100

    Refactor test utility functions
    Replace extensible options with builder patteren.
    
    Signed-off-by: Pierre Crégut <pierre.cregut@orange.com>

--- internal/testing/common.go
+++ internal/testing/common.go
@@ -23,319 +23,340 @@ import (
 	"sigs.k8s.io/cluster-api/util/conditions"
 )
 
-type WithFinalizers []string
-type WithLabels map[string]string
-
-type WithAnnotations map[string]string
-
-type NamespaceOption interface {
-	EnhanceNamespace(*corev1.Namespace)
-}
-
 // Namespace definitions.
-func NewNamespace(name string, options ...NamespaceOption) *corev1.Namespace {
-	ns := &corev1.Namespace{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Namespace",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-	}
-	for _, option := range options {
-		option.EnhanceNamespace(ns)
-	}
-	return ns
-}
 
 const (
 	HostclaimName      = "hostclaim"
 	HostclaimNamespace = "hcNs"
 )
 
-func (wl WithLabels) EnhanceNamespace(ns *corev1.Namespace) {
-	ns.Labels = wl
+type NamespaceBuilder struct {
+	namespace corev1.Namespace
 }
 
-// Secret definitions.
-
-type SecretOption interface {
-	EnhanceSecret(*corev1.Secret)
-}
-
-func NewSecret(name, namespace string, options ...SecretOption) *corev1.Secret {
-	sec := &corev1.Secret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
-		},
-
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-	}
-	for _, option := range options {
-		option.EnhanceSecret(sec)
-	}
-	return sec
-}
-
-type WithData map[string][]byte
-
-func (wd WithData) EnhanceSecret(sec *corev1.Secret) {
-	sec.Data = wd
-}
-
-// BareMetalHost definitions.
-type BaremetalhostOption interface {
-	EnhanceBmh(*metal3api.BareMetalHost)
-}
-
-func NewBaremetalhost(name, namespace string, state metal3api.ProvisioningState, options ...BaremetalhostOption) *metal3api.BareMetalHost {
-	bmh := &metal3api.BareMetalHost{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "BareMetalHost",
-			APIVersion: metal3api.GroupVersion.Identifier(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-		Status: metal3api.BareMetalHostStatus{
-			Provisioning: metal3api.ProvisionStatus{
-				State: state,
+func NewNamespace(name string) *NamespaceBuilder {
+	return &NamespaceBuilder{
+		corev1.Namespace{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Namespace",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
 			},
 		},
 	}
-	for _, option := range options {
-		option.EnhanceBmh(bmh)
+}
+
+func (nb *NamespaceBuilder) SetLabels(labels map[string]string) *NamespaceBuilder {
+	nb.namespace.Labels = labels
+	return nb
+}
+
+func (nb *NamespaceBuilder) Build() *corev1.Namespace {
+	return &nb.namespace
+}
+
+// Secret Builder
+
+type SecretBuilder struct {
+	secret corev1.Secret
+}
+
+func NewSecret(name, namespace string) *SecretBuilder {
+	return &SecretBuilder{
+		corev1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
+			},
+
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+		},
 	}
-	return bmh
 }
 
-func (bl WithLabels) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Labels = bl
+func (sb *SecretBuilder) Build() *corev1.Secret {
+	return &sb.secret
 }
 
-func (ba WithAnnotations) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Annotations = ba
+func (sb *SecretBuilder) SetData(data map[string][]byte) *SecretBuilder {
+	sb.secret.Data = data
+	return sb
 }
 
-type PoweredOnTrue struct{}
-
-func (ot PoweredOnTrue) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.Online = true
+type BareMetalHostBuilder struct {
+	bmh metal3api.BareMetalHost
 }
 
-type WithConsumerRef corev1.ObjectReference
-
-func (wcr WithConsumerRef) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	c := corev1.ObjectReference(wcr)
-	bmh.Spec.ConsumerRef = &c
+func NewBaremetalhost(name, namespace string, state metal3api.ProvisioningState) *BareMetalHostBuilder {
+	return &BareMetalHostBuilder{
+		metal3api.BareMetalHost{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "BareMetalHost",
+				APIVersion: metal3api.GroupVersion.Identifier(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+			Status: metal3api.BareMetalHostStatus{
+				Provisioning: metal3api.ProvisionStatus{
+					State: state,
+				},
+			},
+		},
+	}
 }
 
-type WithUserData string
-
-func (wud WithUserData) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.UserData = &corev1.SecretReference{Name: string(wud)}
+func (bb *BareMetalHostBuilder) Build() *metal3api.BareMetalHost {
+	return &bb.bmh
 }
 
-type WithMetaData string
-
-func (wud WithMetaData) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.MetaData = &corev1.SecretReference{Name: string(wud)}
+func (bb *BareMetalHostBuilder) SetLabels(labels map[string]string) *BareMetalHostBuilder {
+	bb.bmh.Labels = labels
+	return bb
 }
 
-type WithNetworkData string
-
-func (wud WithNetworkData) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.NetworkData = &corev1.SecretReference{Name: string(wud)}
+func (bb *BareMetalHostBuilder) SetAnnotations(annotations map[string]string) *BareMetalHostBuilder {
+	bb.bmh.Annotations = annotations
+	return bb
 }
 
-type WithImage struct {
-	metal3api.Image
+func (bb *BareMetalHostBuilder) SetPowerOn() *BareMetalHostBuilder {
+	bb.bmh.Spec.Online = true
+	return bb
 }
 
-func (wi WithImage) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.Image = &wi.Image
+func (bb *BareMetalHostBuilder) SetConsumerRef(cref corev1.ObjectReference) *BareMetalHostBuilder {
+	bb.bmh.Spec.ConsumerRef = &cref
+	return bb
 }
 
-type WithCleaningMode metal3api.AutomatedCleaningMode
-
-func (wcm WithCleaningMode) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.AutomatedCleaningMode = metal3api.AutomatedCleaningMode(wcm)
+func (bb *BareMetalHostBuilder) SetUserData(udata string) *BareMetalHostBuilder {
+	bb.bmh.Spec.UserData = &corev1.SecretReference{Name: udata}
+	return bb
 }
 
-type WithCustomDeploy string
+func (bb *BareMetalHostBuilder) SetMetaData(mdata string) *BareMetalHostBuilder {
+	bb.bmh.Spec.MetaData = &corev1.SecretReference{Name: mdata}
+	return bb
+}
 
-func (wcd WithCustomDeploy) EnhanceBmh(bmh *metal3api.BareMetalHost) {
-	bmh.Spec.CustomDeploy = &metal3api.CustomDeploy{Method: string(wcd)}
+func (bb *BareMetalHostBuilder) SetNetworData(ndata string) *BareMetalHostBuilder {
+	bb.bmh.Spec.NetworkData = &corev1.SecretReference{Name: ndata}
+	return bb
+}
+
+func (bb *BareMetalHostBuilder) SetImage(image metal3api.Image) *BareMetalHostBuilder {
+	bb.bmh.Spec.Image = &image
+	return bb
+}
+
+func (bb *BareMetalHostBuilder) SetCleaningMode(cmode metal3api.AutomatedCleaningMode) *BareMetalHostBuilder {
+	bb.bmh.Spec.AutomatedCleaningMode = cmode
+	return bb
+}
+
+func (bb *BareMetalHostBuilder) SetCustomDeploy(cd string) *BareMetalHostBuilder {
+	bb.bmh.Spec.CustomDeploy = &metal3api.CustomDeploy{Method: cd}
+	return bb
+}
+
+func (bb *BareMetalHostBuilder) SetCondition(typ string, status bool, reason string) *BareMetalHostBuilder {
+	conditions.Set(&bb.bmh, metav1.Condition{Type: typ, Status: conditions.BoolToStatus(status), Reason: reason})
+	return bb
 }
 
 // Hardware data.
-func NewHardwareData(bmh *metal3api.BareMetalHost) *metal3api.HardwareData {
-	return &metal3api.HardwareData{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "HardwareData",
-			APIVersion: metal3api.GroupVersion.Identifier(),
-		},
 
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      bmh.Name,
-			Namespace: bmh.Namespace,
+type HardwareDataBuilder struct {
+	hardwareData metal3api.HardwareData
+}
+
+func NewHardwareData(bmh *metal3api.BareMetalHost) *HardwareDataBuilder {
+	return &HardwareDataBuilder{
+		metal3api.HardwareData{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "HardwareData",
+				APIVersion: metal3api.GroupVersion.Identifier(),
+			},
+
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      bmh.Name,
+				Namespace: bmh.Namespace,
+			},
+			Spec: metal3api.HardwareDataSpec{
+				HardwareDetails: &metal3api.HardwareDetails{},
+			},
 		},
-		Spec: metal3api.HardwareDataSpec{
-			HardwareDetails: &metal3api.HardwareDetails{},
+	}
+}
+
+func (hb *HardwareDataBuilder) SetLabels(labels map[string]string) *HardwareDataBuilder {
+	hb.hardwareData.Labels = labels
+	return hb
+}
+
+func (hb *HardwareDataBuilder) Build() *metal3api.HardwareData {
+	return &hb.hardwareData
+}
+
+type HostClaimBuilder struct {
+	hostClaim metal3api.HostClaim
+}
+
+func NewHostclaim(name string) *HostClaimBuilder {
+	return &HostClaimBuilder{
+		metal3api.HostClaim{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "HostClaim",
+				APIVersion: metal3api.GroupVersion.Identifier(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: HostclaimNamespace,
+			},
+			Spec: metal3api.HostClaimSpec{
+				HostSelector: metal3api.HostSelector{},
+			},
+		},
+	}
+}
+
+func (hb *HostClaimBuilder) Build() *metal3api.HostClaim {
+	return &hb.hostClaim
+}
+
+func (hb *HostClaimBuilder) SetFinalizer(finalizers []string) *HostClaimBuilder {
+	hb.hostClaim.Finalizers = finalizers
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetLabels(labels map[string]string) *HostClaimBuilder {
+	hb.hostClaim.Labels = labels
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetAnnotations(annotations map[string]string) *HostClaimBuilder {
+	hb.hostClaim.Annotations = annotations
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetConsumerRef(cref corev1.ObjectReference) *HostClaimBuilder {
+	hb.hostClaim.Spec.ConsumerRef = &cref
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetUserData(udata string) *HostClaimBuilder {
+	hb.hostClaim.Spec.UserData = &corev1.SecretReference{Name: udata}
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetMetaData(mdata string) *HostClaimBuilder {
+	hb.hostClaim.Spec.MetaData = &corev1.SecretReference{Name: mdata}
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetNetworData(ndata string) *HostClaimBuilder {
+	hb.hostClaim.Spec.NetworkData = &corev1.SecretReference{Name: ndata}
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetImage(image metal3api.Image) *HostClaimBuilder {
+	hb.hostClaim.Spec.Image = &image
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetCustomDeploy(cd string) *HostClaimBuilder {
+	hb.hostClaim.Spec.CustomDeploy = &metal3api.CustomDeploy{Method: cd}
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetPowerOn() *HostClaimBuilder {
+	hb.hostClaim.Spec.PoweredOn = true
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetFailureDomain(fd string) *HostClaimBuilder {
+	hb.hostClaim.Spec.FailureDomain = fd
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetTargetNamespace(ns string) *HostClaimBuilder {
+	hb.hostClaim.Spec.HostSelector.InNamespace = ns
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetLabelSelector(ls map[string]string) *HostClaimBuilder {
+	hb.hostClaim.Spec.HostSelector.MatchLabels = ls
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetMatchExpressions(me []metal3api.HostSelectorRequirement) *HostClaimBuilder {
+	hb.hostClaim.Spec.HostSelector.MatchExpressions = me
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetCondition(typ string, status bool, reason string) *HostClaimBuilder {
+	conditions.Set(&hb.hostClaim, metav1.Condition{Type: typ, Status: conditions.BoolToStatus(status), Reason: reason})
+	return hb
+}
+
+func (hb *HostClaimBuilder) SetAssociatedBMH(namespace, name string) *HostClaimBuilder {
+	hb.hostClaim.Status.BareMetalHost = &metal3api.ObjectReference{Namespace: namespace, Name: name}
+	return hb
+}
+
+type HostDeployPolicyBuilder struct {
+	hostDeployPolicy metal3api.HostDeployPolicy
+}
+
+func NewHostdeploypolicy(name, namespace string) *HostDeployPolicyBuilder {
+	return &HostDeployPolicyBuilder{
+		metal3api.HostDeployPolicy{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "HostDeployPolicy",
+				APIVersion: metal3api.GroupVersion.Identifier(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
 		},
 	}
 }
 
-// Hostclaim definitions.
-type HostclaimOption interface {
-	EnhanceHostclaim(*metal3api.HostClaim)
+func (hb *HostDeployPolicyBuilder) Build() *metal3api.HostDeployPolicy {
+	return &hb.hostDeployPolicy
 }
 
-func NewHostclaim(name string, options ...HostclaimOption) *metal3api.HostClaim {
-	hc := &metal3api.HostClaim{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "HostClaim",
-			APIVersion: metal3api.GroupVersion.Identifier(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: HostclaimNamespace,
-		},
-		Spec: metal3api.HostClaimSpec{
-			HostSelector: metal3api.HostSelector{},
-		},
+func (hb *HostDeployPolicyBuilder) AcceptNames(acn []string) *HostDeployPolicyBuilder {
+	spec := &hb.hostDeployPolicy.Spec
+	if spec.HostClaimNamespaces == nil {
+		spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
 	}
-	for _, option := range options {
-		option.EnhanceHostclaim(hc)
+	spec.HostClaimNamespaces.Names = acn
+	return hb
+}
+
+func (hb *HostDeployPolicyBuilder) AcceptLabels(acl []metal3api.NameValuePair) *HostDeployPolicyBuilder {
+	spec := &hb.hostDeployPolicy.Spec
+	if spec.HostClaimNamespaces == nil {
+		spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
 	}
-	return hc
+	spec.HostClaimNamespaces.HasLabels = acl
+	return hb
 }
 
-func (wf WithFinalizers) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Finalizers = wf
-}
-
-func (wl WithLabels) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Labels = wl
-}
-
-func (wa WithAnnotations) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Annotations = wa
-}
-
-func (wud WithUserData) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.UserData = &corev1.SecretReference{Name: string(wud)}
-}
-
-func (wud WithMetaData) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.MetaData = &corev1.SecretReference{Name: string(wud)}
-}
-
-func (wud WithNetworkData) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.NetworkData = &corev1.SecretReference{Name: string(wud)}
-}
-
-func (wi WithImage) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.Image = &wi.Image
-}
-
-func (wcd WithCustomDeploy) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.CustomDeploy = &metal3api.CustomDeploy{Method: string(wcd)}
-}
-
-func (ot PoweredOnTrue) EnhanceHostclaim(hc *metal3api.HostClaim) {
-	hc.Spec.PoweredOn = true
-}
-
-func (wcd WithCustomDeploy) EnhanceHostClaim(hc *metal3api.HostClaim) {
-	hc.Spec.CustomDeploy = &metal3api.CustomDeploy{Method: string(wcd)}
-}
-
-type WithTargetNamespace string
-
-func (whs WithTargetNamespace) EnhanceHostclaim(hostclaim *metal3api.HostClaim) {
-	hostclaim.Spec.HostSelector.InNamespace = string(whs)
-}
-
-type WithFailureDomain string
-
-func (wfd WithFailureDomain) EnhanceHostclaim(hostclaim *metal3api.HostClaim) {
-	hostclaim.Spec.FailureDomain = string(wfd)
-}
-
-type WithLabelSelector map[string]string
-
-func (wls WithLabelSelector) EnhanceHostclaim(hostclaim *metal3api.HostClaim) {
-	hostclaim.Spec.HostSelector.MatchLabels = wls
-}
-
-type WithMatchExprSelector []metal3api.HostSelectorRequirement
-
-func (wmes WithMatchExprSelector) EnhanceHostclaim(hostclaim *metal3api.HostClaim) {
-	hostclaim.Spec.HostSelector.MatchExpressions = wmes
-}
-
-type Condition struct {
-	Type   string
-	Status bool
-	Reason string
-}
-
-func (c Condition) EnhanceHostclaim(hostclaim *metal3api.HostClaim) {
-	conditions.Set(hostclaim, metav1.Condition{Type: c.Type, Status: conditions.BoolToStatus(c.Status), Reason: c.Reason})
-}
-
-// HostDeployPolicy definitions.
-type HostdeploypolicyOption interface {
-	EnhanceHDP(*metal3api.HostDeployPolicy)
-}
-
-func NewHostdeploypolicy(name, namespace string, options ...HostdeploypolicyOption) *metal3api.HostDeployPolicy {
-	hdp := &metal3api.HostDeployPolicy{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "HostDeployPolicy",
-			APIVersion: metal3api.GroupVersion.Identifier(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
+func (hb *HostDeployPolicyBuilder) AcceptRegexp(re string) *HostDeployPolicyBuilder {
+	spec := &hb.hostDeployPolicy.Spec
+	if spec.HostClaimNamespaces == nil {
+		spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
 	}
-	for _, option := range options {
-		option.EnhanceHDP(hdp)
-	}
-	return hdp
-}
-
-type AcceptNames []string
-
-func (an AcceptNames) EnhanceHDP(hdp *metal3api.HostDeployPolicy) {
-	if hdp.Spec.HostClaimNamespaces == nil {
-		hdp.Spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
-	}
-	hdp.Spec.HostClaimNamespaces.Names = an
-}
-
-type AcceptLabels []metal3api.NameValuePair
-
-func (al AcceptLabels) EnhanceHDP(hdp *metal3api.HostDeployPolicy) {
-	if hdp.Spec.HostClaimNamespaces == nil {
-		hdp.Spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
-	}
-	hdp.Spec.HostClaimNamespaces.HasLabels = al
-}
-
-type AcceptRegexp string
-
-func (ar AcceptRegexp) EnhanceHDP(hdp *metal3api.HostDeployPolicy) {
-	if hdp.Spec.HostClaimNamespaces == nil {
-		hdp.Spec.HostClaimNamespaces = &metal3api.HostClaimNamespaces{}
-	}
-	hdp.Spec.HostClaimNamespaces.NameMatches = string(ar)
+	spec.HostClaimNamespaces.NameMatches = re
+	return hb
 }
